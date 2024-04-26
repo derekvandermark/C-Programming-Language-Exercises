81cc581b10ea955cda9520681f018bc8e656473e
Derek van der Mark <derek.vandermark@gmail.com>
2024-04-26 14:59:52 -0400

    differentiate between char types in syntax-errors.c

--- Ch1-InProgress/syntax-errors.c
+++ Ch1-InProgress/syntax-errors.c
@@ -2,16 +2,22 @@
 
 #define INQUOTE 1
 #define OUTQUOTE 0
+#define OPENBRACK 1
+#define REGCHAR 0
+#define CLOSEBRACK -1
+#define SQUOTE 2
+#define DQUOTE 3
 #define MAX 1000
 #define ASCIICOUNT 128
 
 /* return char array with ASCII index of closing chars being the opening char value */
 
 void asciipair(char charpairs[]);
+int chartype(int c);
 
 int main()
 {
-    int c, fileidx, openidx, errorsidx, fileline;
+    int c, qstate, cstate, chartype, openidx, errorsidx, fileline;
     char errors[MAX];
     char openchars[MAX];    /* an array of opening characters that have yet to be closed */
     char charpairs[ASCIICOUNT];
@@ -19,11 +25,16 @@ int main()
     asciipair(charpairs);   /* make array for matching brackets/quotes */
 
     while ((c = getchar()) != EOF) {
+	chartype = chartype(c);
+
+	if (chartype == OPENBRACK) {
+
+	}
 
     }
 
 
-    printf("\n\n%s\n\n", allerrors);
+    printf("\n\n%s\n\n", errors);
 }
 
 void asciipair(char charpairs[]) 
@@ -47,5 +58,31 @@ void asciipair(char charpairs[])
     }
 }
 
+int chartype(int c)
+{
+    int i;
+    char open[3];
+    char close[3];
 
+    open[0] = '(';
+    open[1] = '[';
+    open[2] = '{';
+    
+    close[0] = ')';
+    close[1] = ']';
+    close[2] = '}';
 
+    for (i = 0; i < 3; ++i) {
+	if (c == open[i])
+	    return  OPENBRACK;
+	else if (c == close[i])
+	    return CLOSEBRACK;
+	else if (c == '"')
+	    return DQUOTE;
+	else if (c == ''')
+	    return SQUOTE;
+    }
+    
+
+    return REGCHAR;
+}

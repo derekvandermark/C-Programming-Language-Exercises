4da5534f46847d352d8fda4e8d844f3bb3ef0c4d
Derek van der Mark <derek.vandermark@gmail.com>
2024-05-06 18:56:43 -0400

    Ex 2-4 | Improved accuracy of algorithm

--- Ch2-Completed/Ex_2-4.c
+++ Ch2-Completed/Ex_2-4.c
@@ -6,8 +6,8 @@ void squeeze(char s1[], char s2[]);
 
 int main()
 {
-    char str1[] = "This string will have characters removed.";
-    char str2[] = "No more common chars.";
+    char str1[] = "This string will have characters removed.!@#$%^&*()+";
+    char str2[] = "No more common chars.!@#$%";
 
     squeeze(str1, str2);
 
@@ -16,19 +16,17 @@ int main()
 
 void squeeze(char s1[], char s2[])
 {
-    int cs[ASCII];
+    char cs[ASCII];
     int i, j;
 
     for (i = 0; s2[i] != '\0'; i++) 
-	cs[s2[i]] = 1;
+	cs[s2[i]] = 'a';
 
     for (i = j = 0; s1[i] != '\0'; i++) {
-/*
-	printf("%c ", s1[i]);
+/*	printf("%c ", s1[i]);
 	printf("%d\n", cs[s1[i]]);
 */
-
-	if (cs[s1[i]] != 1) {  /* some capital letters that were not in s2 did not have a value of 0, but were billions above or below... hence != -1 */
+	if (cs[s1[i]] != 'a') {  /* some capital letters that were not in s2 did not have a value of 0, but were billions above or below... hence != -1 */
 	    s1[j++] = s1[i];
 	}
     }

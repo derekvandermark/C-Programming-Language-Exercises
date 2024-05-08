28ff064e24bb6fc9d29bc10e937107f8a9ef1838
Derek van der Mark <derek.vandermark@gmail.com>
2024-05-07 21:17:50 -0400

    Bitwise operator playground

---
+++ Playgrounds/bitwise-operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 9;
+
+    printf("Bitwise AND test: %d\n", a & b);
+
+    int c = 8;
+    int d = 15;
+
+    printf("Bitwise inclusive OR test: %d\n", c | d);
+
+    int e = 19;
+    int f = 7;
+
+    printf("Bitwise exclusive OR test: %d\n", e ^ f);
+
+
+}

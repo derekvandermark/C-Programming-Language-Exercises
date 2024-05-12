bfc50164da082c70468afea9e38888f72c3c06d5
Derek van der Mark <derek.vandermark@gmail.com>
2024-05-11 22:17:33 -0400

    Complete Ex 2.6

--- Ch2-InProgress/Ex_2-6.c
+++ Ch2-InProgress/Ex_2-6.c
@@ -13,9 +13,9 @@ unsigned setbits(unsigned x, int p, int n, unsigned y)
 {
     int rightmost_n = ~(~0 << n);   /* rightmost n bits set to 1 */
     int nofy = y & rightmost_n;
-    int nofy_at_p = nofy << p;
-    int left_of_p = ~0 << p;
-    int right_of_p = ~(~0 << (p - n));
+    int nofy_at_p = nofy << (p - n + 1);
+    int left_of_p = ~0 << (p + 1);
+    int right_of_p = ~(~0 << (p - n + 1));
     int not_p = left_of_p ^ right_of_p; /* all bits 1 except from p to p - n */
     nofy_at_p = nofy_at_p ^ not_p;
     return x & nofy_at_p; 

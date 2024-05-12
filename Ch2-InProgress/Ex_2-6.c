bd686f5a7c726acaa4a9d23c1e0879bb9649e34f
Derek van der Mark <derek.vandermark@gmail.com>
2024-05-11 21:59:29 -0400

    Begin bitwise exercises

---
+++ Ch2-InProgress/Ex_2-6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+
+int main()
+{
+    int res = setbits(93, 4, 3, 198);
+
+    printf("%d\n", res);
+}
+
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+    int rightmost_n = ~(~0 << n);   /* rightmost n bits set to 1 */
+    int nofy = y & rightmost_n;
+    int nofy_at_p = nofy << p;
+    int left_of_p = ~0 << p;
+    int right_of_p = ~(~0 << (p - n));
+    int not_p = left_of_p ^ right_of_p; /* all bits 1 except from p to p - n */
+    nofy_at_p = nofy_at_p ^ not_p;
+    return x & nofy_at_p; 
+}
